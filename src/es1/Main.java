a27181ec5d9946d78af4e0a8c880b16de94ea169
ariannaal <ariannaloreti25@gmail.com>
2024-07-29 15:55:32 +0200

    adds es 1 and es 2

---
+++ src/es1/Main.java
@@ -0,0 +1,12 @@
+package es1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = new int[5];
+        Esercizio1 esercizio1 = new Esercizio1(array);
+        esercizio1.esercizio1();
+        esercizio1.start();
+
+    }
+}
